49f3593563678cadfca971412462a66c1968b8a2
Elmo <elmovg_99@hotmail.com>
2019-06-07 01:19:12 -0500

    Primer switch

--- eddy es chingo.cpp
+++ eddy es chingo.cpp
@@ -19,3 +19,41 @@ class persona{
         }
 		
 };
+
+int main(){
+	do{
+		cout<<endl<<"1.CLIENTES"<<endl;
+		cout<<"2.PELICULAS"<<endl;
+		cout<<"3.EMPLEADO"<<endl;
+		cout<<"4.REALIZAR RENTA O VENTA"<<endl;
+		cout<<"5.SALIR"<<endl;
+		cout<<endl<<"Opcion: ";
+		opc=getche();
+		switch(opc)
+		{
+			case'1':
+
+				break;
+			case'2':
+
+				break;
+			case'3':
+
+				break;
+			case'4':
+				
+				break;
+			case'5':
+				cout<<endl<<"Adios"<<endl;
+				break;
+			default:
+				cout<<endl<<"Error"<<endl;
+				break;
+		}
+		getch();
+	} while(opc!='4');
+	return 0;
+	
+}
+
+
